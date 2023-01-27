16967a45fc64768fc3855d6bab43b87289f4bff6
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-01-27 15:11:56 +0300

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,34 @@
+import twitUrl from "../assets/Twitter.png";
+import fbUrl from "../assets/Facebook.png";
+import instUrl from "../assets/instagram.png";
+import gitUrl from "../assets/GitHub.png";
+import "./Footer.css";
+
 export default function Footer() {
   return (
-    <h1> Footer goes here</h1>
-  )
+    <section className="Footer">
+      <ul>
+        <li>
+          <a href="https://twitter.com">
+            <img src={twitUrl} alt="twitter logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://facebook.com">
+            <img src={fbUrl} alt="facebook logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://instagram.com/denis_kyznecov">
+            <img src={instUrl} alt="instagram logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/DenKuznets">
+            <img src={gitUrl} alt="github logo" />
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
 }
