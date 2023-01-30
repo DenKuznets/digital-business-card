8a6b097f23c0ab3064221b1731e577cfd8d84bd5
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-30 09:59:01 +0300

    added linkedin url

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,11 +9,18 @@ export default function Info() {
     <section className="info">
       <img src={avatarUrl} className="info--img filter" alt="avatar" />
       <h1 className="info--h1">Denis Kuznetsov</h1>
-      <h3 className="info--h3">Trainee Frontend Developer</h3>      
-      <a className='info--a' href="https://github.com/DenKuznets">denkuznets.Github</a>
-      <div className='info--buttons-container'>
-        <a href='mailto: denis.kyznecov@gmail.com' className='email'><img src={maillogoUrl} alt='mail-icon' /> Email</a>
-        <a href='' className='linkedin'><img src={lkdnLogoUrl} alt="linkedin-icon" />Linkedin</a>
+      <h3 className="info--h3">Trainee Frontend Developer</h3>
+      <a className="info--a" href="https://github.com/DenKuznets">
+        denkuznets.Github
+      </a>
+      <div className="info--buttons-container">
+        <a href="mailto: denis.kyznecov@gmail.com" className="email">
+          <img src={maillogoUrl} alt="mail-icon" /> Email
+        </a>
+        <a href="www.linkedin.com/in/DenKuznets" className="linkedin">
+          <img src={lkdnLogoUrl} alt="linkedin-icon" />
+          Linkedin
+        </a>
       </div>
     </section>
   );
